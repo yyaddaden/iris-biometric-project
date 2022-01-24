726e896e9cf9f94df82e27e8618058f6f3bd91bc
Yaddaden Yacine <yaddya01@uqar.ca>
2022-01-24 13:32:07 -0500

    add cleaned dataset, references and report.

---
+++ Source Code/Iris.py
@@ -0,0 +1,6 @@
+# -*- coding: UTF-8 -*-
+
+class Iris:
+    def __init__(self):
+        #####
+        pass
---
+++ Source Code/run.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+
+from Iris import *
+
+if __name__ == '__main__':
+    #####
+    pass
