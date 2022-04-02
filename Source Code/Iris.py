475aa968dd74800333286c225c570bab8a0de2be
François Beaulieu <beaulieuf2000@hotmail.com>
2022-04-02 16:52:51 -0400

    csv, pca and knn

--- Source Code/Iris.py
+++ Source Code/Iris.py
@@ -14,6 +14,8 @@ from PIL import Image
 from pathlib import Path
 from skimage.color import rgb2gray
 from sklearn.decomposition import PCA
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import StratifiedKFold
 
 class Iris:
     def __init__(self):
@@ -83,21 +85,34 @@ class Iris:
         base_path = Path(__file__)
         path = (base_path / "../Prepared-MMU-Iris-Database/").resolve()
 
-        features = []
+        GLCMs_1D = []
 
         for f in os.listdir(path):
             for c in os.listdir(os.path.join(path, f)):
                 for i in os.listdir(os.path.join(path, f, c)):
-                    features.append(self.glcm(os.path.join(path, f, c, i)))
+                    GLCMs_1D.append(self.glcm(os.path.join(path, f, c, i)))
 
-        self.pca(features)
+        PCA = []
+        PCA = self.pca(GLCMs_1D)
 
-        with open('glcm.csv', 'w', encoding='UTF8', newline='') as f:
-            # create the csv writer
-            writer = csv.writer(f)
-            for row in features:
-                # write a row to the csv file
-                writer.writerow(row)
+        self.classification(PCA)
+
+        #number_of_file = int(len(PCA)/50)
+
+        #for i in range(number_of_file):
+        #    PCA_components = [[]]*450
+        #    index = 0
+        #    for j in PCA:
+        #        PCA_components[index] = list(j[(i*50):][:50])
+        #        PCA_components[index].append(int(index/10))
+        #        index += 1
+
+        #    with open('glcm' + str(i) + '.csv', 'w', encoding='UTF8', newline='') as f:
+        #        # create the csv writer
+        #        writer = csv.writer(f)
+        #        for row in PCA_components:
+        #            # write a row to the csv file
+        #            writer.writerow(row)                
 
 
     def glcm(self, file_path):
@@ -113,14 +128,30 @@ class Iris:
 
         return glcm_1D.tolist()
 
-    def pca(self, features):
-        pca = PCA()
-        pca.fit(features)
-        #print(pca.explained_variance_ratio_)
-        #print(pca.singular_values_)
+    def pca(self, GLCMs_1D):
+        pca = PCA(n_components=(min(len(GLCMs_1D), len(GLCMs_1D[0]))))
+        pca.fit(GLCMs_1D)
+        return pca.components_
 
     def classification(self, data):
-        # X, y ?
+        X = data
+        y = []*450
+
+        for i in range(45):
+            for j in range(10):
+                y.append(i)
+
+        y = np.array(y)
+
         knn = KNeighborsClassifier(n_neighbors=3)
-        scores = cross_val_score(knn, X, y, cv=10)
-        print(scores)
+        
+        skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=10)
+        k_folds = skf.split(X, y)
+
+        for train_index, test_index in k_folds:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            knn.fit(X_train, y_train)
+            print(y_test)
+            print(knn.predict(X_test))
+
