7a8d4defc589c949a9b13b989df4938e6909319b
François Beaulieu <beaulieuf2000@hotmail.com>
2022-04-30 18:48:53 -0400

    re-arrange and comment the code and update the report

--- Source Code/Iris.py
+++ Source Code/Iris.py
@@ -20,18 +20,22 @@ from sklearn.model_selection import StratifiedKFold
 class Iris:
     def __init__(self):
         #####
-        #matplotlib.use('Agg')
         pass
 
-    def process_data(self):
+
+    def pre_process_data(self):
+        matplotlib.use('Agg')
+
+        # Pre-process all images
         base_path = Path(__file__)
         path = (base_path / "../MMU-Iris-Database/").resolve()
         for f in os.listdir(path):
             for c in os.listdir(os.path.join(path, f)):
                 for i in os.listdir(os.path.join(path, f, c)):
-                    self.process_image(os.path.join(path, f, c, i))
+                    self.pre_process_image(os.path.join(path, f, c, i))
 
-    def process_image(self, file_path):
+
+    def pre_process_image(self, file_path):
         # Load picture and detect edges
         image = np.array(Image.open(file_path), dtype=np.uint8)
         image = rgb2gray(image)
@@ -54,6 +58,7 @@ class Iris:
                                             shape=image.shape)
             image[circy, circx] = (220, 20, 20)
 
+        # Transform iris into a rectangle image
         warped = warp_polar(image, (cy[0], cx[0]), radius=radii[0], output_shape=(image.shape[1]*2, image.shape[0]*2), 
             scaling='linear', channel_axis=2)
 
@@ -63,6 +68,7 @@ class Iris:
 
         cropped_warped_image = []
 
+        # Remove the black portion of the image (pupil)
         for x in range(warped_image.height):
             black_percentage = 0
             black_number = 0
@@ -74,6 +80,7 @@ class Iris:
             if black_percentage < 0.5:
                 cropped_warped_image.append(warped[x])
 
+        # Save pre-processed images
         new_file_path = file_path.replace("MMU-Iris-Database", "Prepared-MMU-Iris-Database")
 
         if not os.path.exists(new_file_path.rsplit('\\',1)[0]):
@@ -81,7 +88,7 @@ class Iris:
         Image.fromarray((np.array(cropped_warped_image) * 255).astype(np.uint8)).save(new_file_path)
 
 
-    def create_glcm(self):
+    def evaluation(self, bool_sample, n_folds=10, n_knn=2):
         base_path = Path(__file__)
         path = (base_path / "../Prepared-MMU-Iris-Database/").resolve()
 
@@ -89,28 +96,106 @@ class Iris:
         GLCMs_1D = []
         Labels = []
 
+        # Create 1D GLCM of all images
         for f in os.listdir(path):
-            if f in sample:
+            if not bool_sample or f in sample:
                 for c in os.listdir(os.path.join(path, f)):
                     for i in os.listdir(os.path.join(path, f, c)):
                         GLCMs_1D.append(self.glcm(os.path.join(path, f, c, i)))
                         Labels.append(f)
 
+        # dimentionality reduction
         PCA = []
         PCA = self.pca(GLCMs_1D, min(len(GLCMs_1D), len(GLCMs_1D[0])))
 
-        self.classification(PCA, Labels)
+        # evaluation with cross validation
+        self.cross_validation(PCA, Labels, n_folds, n_knn)
 
-        #self.create_glcm_pca_csv(GLCMs_1D, Labels)
 
-    
-    def create_glcm_pca_csv(self, GLCMs_1D, Labels):
-        number_of_file = 10
+    def glcm(self, file_path):
+        # Load image
+        image = np.array(Image.open(file_path), dtype=np.uint8)
+        image = rgb2gray(image)
+        image = Image.fromarray((image * 255).astype(np.uint8))
+        image = np.array(image, dtype=np.uint8)
+
+        # Create 1D GLCM of the image
+        glcm = graycomatrix(image, distances=[10], angles=[0], levels=256)
+        glcm_1D = glcm[:, :, 0, 0].flatten()
+
+        return glcm_1D.tolist()
+
+
+    def pca(self, GLCMs_1D, number_of_components):
+        # Apply dimensionality reduction
+        pca = PCA(n_components=number_of_components)
+        return pca.fit_transform(GLCMs_1D)
+
+
+    def cross_validation(self, data, labels, n_folds, n_knn):
+        X = data
+        y = np.array(labels)
+
+        # Create knn classifier
+        # p=1 for manhattan distance
+        knn = KNeighborsClassifier(n_neighbors=n_knn, weights='distance', p=1)
+        
+
+        skf = StratifiedKFold(n_splits=n_folds, shuffle=True, random_state=10)
+        k_folds = skf.split(X, y)
+
+        n = 0
+        score_sum = 0
+
+        # Evaluation with cross validation
+        for train_index, test_index in k_folds:
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+            knn.fit(X_train, y_train)
+            score_sum += knn.score(X_test, y_test)
+            n += 1
+
+        # print recognition rate
+        print("Recognition rate: {0:.00%}".format(score_sum/n))
+
+
+    def export_glcm_pca_to_csv(self, bool_sample):
+        base_path = Path(__file__)
+        path = (base_path / "../Prepared-MMU-Iris-Database/").resolve()
+
+        sample = ["005", "010", "013", "022", "027", "030", "032", "035", "038", "040"]
+        GLCMs_1D = []
+        Labels = []
+
+        # Create 1D GLCM of all images
+        for f in os.listdir(path):
+            if not bool_sample or f in sample:
+                for c in os.listdir(os.path.join(path, f)):
+                    for i in os.listdir(os.path.join(path, f, c)):
+                        GLCMs_1D.append(self.glcm(os.path.join(path, f, c, i)))
+                        Labels.append(f)
+
+        # export 1D GLCMs to csv files with different number of components
+        self.create_glcm_pca_csv_files(GLCMs_1D, Labels, bool_sample)
+
+
+    def create_glcm_pca_csv_files(self, GLCMs_1D, Labels, bool_sample):
+
+        if bool_sample:
+            number_of_file = 10
+        else:
+            number_of_file = 9
 
         for i in range(number_of_file):
             PCA_components = []
             header = []
-            n_components=(i+1)*10
+
+            if bool_sample:
+                n_components=(i+1)*10
+            else:
+                n_components=(i+1)*50
+            
+            # Apply dimensionnality reduction
             PCA_components = self.pca(GLCMs_1D, n_components)
         
             for j in range(len(PCA_components[0])):
@@ -120,10 +205,17 @@ class Iris:
         
             PCA_components_labels = [[]]*len(PCA_components)
         
+            # Add IDs to the PCA array
             for j in range(len(PCA_components)):
                 PCA_components_labels[j] = np.append(PCA_components[j], Labels[j]).tolist()
-        
-            with open('glcm-sample' + str(i) + '.csv', 'w', encoding='UTF8', newline='') as f:
+
+            file_name = "glcm"
+
+            if bool_sample:
+                file_name += "_sample"
+
+            # Create csv file
+            with open(file_name + str(i) + '.csv', 'w', encoding='UTF8', newline='') as f:
                 # create the csv writer
                 writer = csv.writer(f)
         
@@ -131,53 +223,10 @@ class Iris:
         
                 for row in PCA_components_labels:
                     # write a row to the csv file
-                    writer.writerow(row)                
+                    writer.writerow(row)     
 
 
-    def glcm(self, file_path):
-        image = np.array(Image.open(file_path), dtype=np.uint8)
-        image = rgb2gray(image)
-        image = Image.fromarray((image * 255).astype(np.uint8))
-        image = np.array(image, dtype=np.uint8)
-        #glcm = graycomatrix(image, distances=[5], angles=[0], levels=256)
-        glcm = graycomatrix(image, distances=[10], angles=[0], levels=256)
-        glcm_1D = glcm[:, :, 0, 0].flatten()
-
-        #plt.imshow(glcm[:, :, 0, 0])
-        #plt.show()
-
-        return glcm_1D.tolist()
-
-    def pca(self, GLCMs_1D, number_of_components):
-        pca = PCA(n_components=number_of_components)
-        return pca.fit_transform(GLCMs_1D)
-
-    def classification(self, data, labels):
-        X = data
-        y = np.array(labels)
-
-        # p=1 for manhattan distance
-        knn = KNeighborsClassifier(n_neighbors=2, weights='distance', p=1)
-        
-        skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=10)
-        k_folds = skf.split(X, y)
-
-        n = 0
-        score_sum = 0
-
-        for train_index, test_index in k_folds:
-            X_train, X_test = X[train_index], X[test_index]
-            y_train, y_test = y[train_index], y[test_index]
-            knn.fit(X_train, y_train)
-            #print(y_test)
-            #print(knn.predict(X_test))
-            #print(knn.score(X_test, y_test))
-            score_sum += knn.score(X_test, y_test)
-            n += 1
-
-        print(score_sum/n)
-            
-    def glcm_features(self):
+    def create_glcm_features_csv_files(self, bool_sample):
         base_path = Path(__file__)
         path = (base_path / "../Prepared-MMU-Iris-Database/").resolve()
 
@@ -187,18 +236,23 @@ class Iris:
 
         GLCM_features[0] = ["contrast", "dissimilarity", "homogeneity", "energy", "correlation", "ASM", "ID"]
 
+        # Create array of GLCM features for all images
         for f in os.listdir(path):
-            if f in sample:
+            if not bool_sample or f in sample:
                 for c in os.listdir(os.path.join(path, f)):
                     for i in os.listdir(os.path.join(path, f, c)):
+                        # Load image
                         image = np.array(Image.open(os.path.join(path, f, c, i)), dtype=np.uint8)
                         image = rgb2gray(image)
                         image = Image.fromarray((image * 255).astype(np.uint8))
                         image = np.array(image, dtype=np.uint8)
+
+                        # Create GLCM
                         glcm = graycomatrix(image, distances=[10], angles=[0], levels=256)
 
                         GLCM_features.append([])
 
+                        # Extract GLCM texture features
                         GLCM_features[index].append(graycoprops(glcm, 'contrast')[0][0])
                         GLCM_features[index].append(graycoprops(glcm, 'dissimilarity')[0][0])
                         GLCM_features[index].append(graycoprops(glcm, 'homogeneity')[0][0])
@@ -209,7 +263,13 @@ class Iris:
 
                         index += 1
 
-        with open('glcm_features_sample.csv', 'w', encoding='UTF8', newline='') as f:
+        file_name = "glcm_features"
+
+        if bool_sample:
+            file_name += "_sample"
+
+        # Create csv file
+        with open(file_name + '.csv', 'w', encoding='UTF8', newline='') as f:
                 # create the csv writer
                 writer = csv.writer(f)
         
--- Source Code/run.py
+++ Source Code/run.py
@@ -1,18 +1,53 @@
 # -*- coding: UTF-8 -*-
 
 from Iris import *
-#from test import *
+
+from argparse import ArgumentParser
 
 if __name__ == '__main__':
     #####
-    #test1 = test()
-    #test1.find_iris_loop() 
-    #test1.iris_detection() 
-    iris = Iris()
-    #iris.process_data()
-    
-    
-    iris.create_glcm()
-    #iris.glcm_features()
 
-    pass
+    # Parameters management
+    args_parser = ArgumentParser(
+        description="Iris-based biometric identification system"
+    )
+    args_parser.version = "1.0"
+
+    group = args_parser.add_argument_group("data pre-processing")
+    group.add_argument("-p", "--preprocessing", help="pre-processing", action="store_true")
+
+    group = args_parser.add_argument_group("evaluation")
+    group.add_argument("-e", "--evaluation", help="evaluation", action="store_true")
+    group.add_argument("-s", "--sample", help="evaluation with sample of 10 people", action="store_true")
+    group.add_argument("-f", "--folds", metavar="", help="number of folds", action="store")
+    group.add_argument("-k", "--knn", metavar="", help="number of neighbors", action="store")
+
+    group = args_parser.add_argument_group("export to csv files")
+    group.add_argument("-c", "--csv", help="export to csv", action="store_true")
+    group.add_argument("-tf", "--texturefeatures", help="export GLCM texture features", action="store_true")
+
+    args = args_parser.parse_args()
+
+    # Parameters validation & execution
+    iris = Iris()
+
+    if args.preprocessing:
+        iris.pre_process_data()
+    elif args.evaluation:
+        if args.folds is None:
+            if args.knn is None:
+                iris.evaluation(args.sample)
+            else:
+                iris.evaluation(args.sample, n_knn=int(args.knn))
+        else:
+            if args.knn is None:
+                iris.evaluation(args.sample, int(args.folds))
+            else:
+                iris.evaluation(args.sample, int(args.folds), int(args.knn))
+    elif args.csv:
+        if args.features:
+            iris.export_glcm_pca_to_csv(args.sample)
+        else:
+            iris.create_glcm_features_csv_files(sample)
+    else:
+        args_parser.print_help()
